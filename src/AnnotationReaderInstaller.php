022fb006ae2c3760302e6bcd627bb7a035304a09
camkLcbr <cam.koppel@gmail.com>
2014-04-22 17:09:30 +0200

    Rename Installer class

--- src/AnnotationReaderInstaller.php
+++ src/AnnotationReaderInstaller.php
@@ -9,6 +9,7 @@ namespace Mouf\Utils\Common\Doctrine\Annotations;
 
 use Mouf\Installer\PackageInstallerInterface;
 use Mouf\MoufManager;
+use Mouf\Actions\InstallUtils;
 
 /**
  * A logger class that writes messages into the php error_log.
@@ -21,7 +22,7 @@ class AnnotationReaderInstaller implements PackageInstallerInterface {
 	 */
 	public static function install(MoufManager $moufManager) {
 		// Let's create the instances.
-		$annotationReader = InstallUtils::getOrCreateInstance('annotationReader', 'Doctrine\\Common\\Annotations\\CachedReader', $moufManager);
+		$annotationReader = InstallUtils::getOrCreateInstance('annotationReader', null, $moufManager);
 		$annotationReader->setCode('// Using composer autoloader
 $loader = require ROOT_PATH . \'vendor/autoload.php\';
 Doctrine\\Common\\Annotations\\AnnotationRegistry::registerLoader(array($loader, \'loadClass\'));
